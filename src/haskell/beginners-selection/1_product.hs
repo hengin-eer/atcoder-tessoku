d6a058107745f9a6c93e97d91029377c5d649570
timdaik <hirokix0608@gmail.com>
2025-08-04 13:52:03 +0900

    🐛 解答例を写経 取り敢えず`map read ...`は覚えよう

--- src/haskell/beginners-selection/1_product.hs
+++ src/haskell/beginners-selection/1_product.hs
@@ -1,9 +1,13 @@
 -- https://atcoder.jp/contests/abs/tasks/abc086_a?lang=ja
 main :: IO ()
 main = do
-  a <- readLn :: IO Int
-  b <- readLn :: IO Int
-  let product x y = x * y :: Int -> Int -> Int
-  let isEvenOrOdd :: Int -> String
-      isEvenOrOdd n = if even n then "Even" else "Odd"
-  putStrLn $ isEvenOrOdd (product a b)
+  -- a <- readLn :: IO Int
+  -- b <- readLn :: IO Int
+  -- let product x y = x * y :: Int -> Int -> Int
+  -- let isEvenOrOdd :: Int -> String
+  --     isEvenOrOdd n = if even n then "Even" else "Odd"
+  -- putStrLn $ isEvenOrOdd (product a b)
+
+  [a, b] <- map read . words <$> getLine :: IO [Int]
+  putStrLn $ let x = a * b in if even x then "Even" else "Odd"
+
